ceeb07f5523aaffa5d8a7927bd74b4f7f8408378
Natali <nataly14balverde@gmail.com>
2025-05-05 18:38:01 -0500

    init project

---
+++ src/main/java/com/cat/msa/msa/invoices/MsaInvoicesApplication.java
@@ -0,0 +1,13 @@
+package com.cat.msa.msa.invoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaInvoicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaInvoicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cat/msa/msa/invoices/MsaInvoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat.msa.msa.invoices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaInvoicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
