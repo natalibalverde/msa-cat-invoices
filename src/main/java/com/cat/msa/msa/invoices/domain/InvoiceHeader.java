6306489995ee3b946cf343d17ff22c4434f66587
Natali <nataly14balverde@gmail.com>
2025-05-14 17:55:40 -0500

    feat: map entities

--- src/main/java/com/cat/msa/msa/invoices/domain/InvoiceHeader.java
+++ src/main/java/com/cat/msa/msa/invoices/domain/InvoiceHeader.java
@@ -1,6 +1,7 @@
 package com.cat.msa.msa.invoices.domain;
 
 import com.cat.msa.msa.invoices.constant.Constant;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import java.math.BigDecimal;
@@ -9,15 +10,34 @@ import java.util.List;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "T_INVOICE_HEADERS")
 public class InvoiceHeader {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "INH_ID", nullable = false)
     private Long id;
+
+    @Column(name = "INH_NUMBER", nullable = false)
     private String number;
+
+    @Column(name = "INH_CUS_NAME", nullable = false)
     private String customerName;
+
+    @Column(name = "INH_DATE", nullable = false)
     private Date date;
+
+    @Column(name = "INH_SUB_TOTAL", nullable = false)
     private BigDecimal subTotalAmount;
+
+    @Column(name = "INH_VAT_AMOUNT", nullable = false)
     private BigDecimal vatAmount;
+
+    @Column(name = "INH_TOTAL", nullable = false)
     private BigDecimal totalAmount;
+
+    @OneToMany(mappedBy = "invoiceHeader", cascade = CascadeType.ALL)
     private List<InvoiceDetail> invoiceDetails;
 
     public void calculateSubTotalAmount(){
