dc0dd48aee2aa7b03a0b117d3111564b7c4e4792
Andres G <gallegosandres.tareas@gmail.com>
2025-05-17 15:40:57 -0500

    feat: create endpoint listInvoices

--- src/main/java/com/cat/msa/msa/invoices/controller/InvoiceHeaderApi.java
+++ src/main/java/com/cat/msa/msa/invoices/controller/InvoiceHeaderApi.java
@@ -2,14 +2,20 @@ package com.cat.msa.msa.invoices.controller;
 
 import com.cat.msa.msa.invoices.domain.InvoiceHeader;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/api/v1/invoice-header")
 public interface InvoiceHeaderApi {
 
     @PostMapping
     ResponseEntity<InvoiceHeader> save(@RequestBody InvoiceHeader invoiceHeader);
+
+    @GetMapping
+    ResponseEntity<List<InvoiceHeader>> findAll();
 
 }
