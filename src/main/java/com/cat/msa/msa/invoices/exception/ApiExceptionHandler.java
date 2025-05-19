10777907cde824288a406570e779277a720acc74
Andres G <gallegosandres.tareas@gmail.com>
2025-05-19 00:51:16 -0500

    feat: create exeptions for InvoiceHeaderServiceImpl

---
+++ src/main/java/com/cat/msa/msa/invoices/exception/ApiExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.cat.msa.msa.invoices.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> runtimeExceptionHandler(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+}
--- src/main/java/com/cat/msa/msa/invoices/service/impl/InvoiceHeaderServiceImpl.java
+++ src/main/java/com/cat/msa/msa/invoices/service/impl/InvoiceHeaderServiceImpl.java
@@ -18,17 +18,29 @@ public class InvoiceHeaderServiceImpl implements InvoiceHeaderService {
 
     @Override
     public InvoiceHeader createInvoiceHeader(InvoiceHeader invoiceHeader) {
+        InvoiceHeader expectedInvoiceHeader = invoiceHeaderRepository.findByNumber(invoiceHeader.getNumber());
+        if (expectedInvoiceHeader != null) {
+            throw new RuntimeException("An invoice with this number already exists");
+        }
         invoiceHeader.calculateInvoiceAmounts();
         return invoiceHeaderRepository.save(invoiceHeader);
     }
 
     @Override
     public List<InvoiceHeader> getAllInvoiceHeaders() {
-        return invoiceHeaderRepository.findAll();
+        List<InvoiceHeader> expectedInvoiceHeaders = invoiceHeaderRepository.findAll();
+        if (expectedInvoiceHeaders.isEmpty()) {
+            throw new RuntimeException("No invoices found");
+        }
+        return expectedInvoiceHeaders;
     }
 
     @Override
     public InvoiceHeader getInvoiceHeaderByNumber(String number) {
-        return invoiceHeaderRepository.findByNumber(number);
+        InvoiceHeader expectedInvoiceHeader = invoiceHeaderRepository.findByNumber(number);
+        if (expectedInvoiceHeader == null) {
+            throw new RuntimeException("Invoice number not found");
+        }
+        return expectedInvoiceHeader;
     }
 }
